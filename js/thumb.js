18aaf83e8a9f77fc145ac94abe68fa3e6825ef67
addisonfreeman <afree1423@gmail.com>
2018-10-16 10:15:21 -0500

    update deer profiles and style

--- js/thumb.js
+++ js/thumb.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
   $('.owl-carousel').owlCarousel({
     thumbs: true,
     thumbsPrerendered: true,
-    items:1
+    items:1,
+    slideSpeed: 3000,
+    paginationSpeed: 1000
   });
 });
